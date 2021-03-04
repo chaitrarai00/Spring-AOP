134ff89268616cb8700ba9a8ac96da1dc48e7168
chaitrarai00 <chaitrarai00@gmail.com>
2021-03-04 09:29:39 +0530

    AOP concepts

--- src/main/java/com/spring/aspects/EmployeeAroundAspect.java
+++ src/main/java/com/spring/aspects/EmployeeAroundAspect.java
@@ -1,5 +1,26 @@
 package com.spring.aspects;
 
-public class EmployeeAroundAspect {
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
+@Aspect
+public class EmployeeAroundAspect {
+	/*
+	 * Around has this feature of controlling the behaviour of the method it targets 
+	 * thats the joinpoint and also the feature to execute or not execute that method
+	 */
+	@Around("execution (* com.spring.model.Employee.getName())")
+	public Object EmpAroundAdvice(ProceedingJoinPoint proceedingJoinPoint) {
+		Object value=null;
+		System.out.println("Before invoking Around targetted method");
+		try {
+			value=proceedingJoinPoint.proceed();
+			// this argument lets you call the targeting joinpoint method and returns the value
+		}catch(Throwable e) {
+		e.printStackTrace();
+		}
+		System.out.println("After invoking Around targetted method");
+		return value;
+	}
 }
